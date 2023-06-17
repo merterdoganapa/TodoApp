6ff4f64223c245d9a5a4c0ee734726a590c60412
merterdoganapa <69794107+merterdoganapa@users.noreply.github.com>
2023-06-17 19:49:07 +0300

    Create Navigation Graph (#3)

--- app/src/main/java/com/mea/todoapp/ui/MainActivity.kt
+++ app/src/main/java/com/mea/todoapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mea.todoapp
+package com.mea.todoapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mea.todoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/mea/todoapp/ui/tasks/TasksFragment.kt
@@ -0,0 +1,8 @@
+package com.mea.todoapp.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.mea.todoapp.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks){
+
+}
